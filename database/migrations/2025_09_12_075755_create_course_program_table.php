85f1191812829de45d1829097764ffc5e94b3bd0
John Adrian Cruz <jhndrncrz@gmail.com>
2025-09-12 18:21:53 +0800

    course program table

---
+++ database/migrations/2025_09_12_075755_create_course_program_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_program', function (Blueprint $table) {
+            $table->id();
+            
+            // Foreign keys
+            $table->foreignId('course_id')->constrained('courses', 'id')->onDelete('cascade');
+            $table->foreignId('program_id')->constrained('programs', 'id')->onDelete('cascade');
+            
+            // Ensure unique combination
+            $table->unique(['course_id', 'program_id']);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_program');
+    }
+};
