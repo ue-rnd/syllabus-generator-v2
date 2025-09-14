dfa642c4b1eafacb400415bf9c94190996312a22
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-09-14 17:56:16 +0800

    removed verify-email routes as it is not needed

--- routes/auth.php
+++ routes/auth.php
@@ -17,17 +17,5 @@ Route::middleware('guest')->group(function () {
     Route::post('reset-password/{token}', [ResetPassword::class, 'reset'])->name('password.update');
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('verify-email', VerifyEmail::class)
-        ->name('verification.notice');
-
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-        ->middleware(['signed', 'throttle:6,1'])
-        ->name('verification.verify');
-
-    Route::get('confirm-password', ConfirmPassword::class)
-        ->name('password.confirm');
-});
-
 Route::get('logout', App\Livewire\Actions\Logout::class)
     ->name('logout');
