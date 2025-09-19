b3ab5a48a111edf9b5cd86285a3a1e4b0f340b21
micoh-villar <villar.micoh3982@gmail.com>
2025-09-19 13:10:43 +0800

    hotfix: root route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('login');
-})->name('home');
+})->name('root');
 
 
 Route::middleware(['auth'])->group(function () {
