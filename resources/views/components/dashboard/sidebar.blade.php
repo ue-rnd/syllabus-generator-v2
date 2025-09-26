6d034035af01de6effdd6e458740fcd8d0a5f05e
JohnGabb12 <johngabvalencerina@gmail.com>
2025-09-26 17:35:02 +0800

    Syllabus icons redesign and tooltip

--- resources/views/components/dashboard/sidebar.blade.php
+++ resources/views/components/dashboard/sidebar.blade.php
@@ -11,22 +11,25 @@
 		</span>
 	</a>
 	<a href="{{ route('home') }}" class="hover:bg-gray-200 p-2 rounded ease duration-200 group relative">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
-			<path fill-rule="evenodd"
-				d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
-				clip-rule="evenodd" />
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+			<path
+				d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+			<path
+				d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
 		</svg>
+
 		<span
 			class="border shadow pointer-events-none select-none absolute left-full top-1/2 -translate-y-1/2 ml-2 w-max bg-white text-black text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition font-bold whitespace-nowrap z-10">
 			Home
 		</span>
 	</a>
 	<a href="{{ route('notifications') }}" class="hover:bg-gray-200 p-2 rounded ease duration-200 group relative">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
 			<path fill-rule="evenodd"
-				d="M10 2a6 6 0 0 0-6 6c0 1.887-.454 3.665-1.257 5.234a.75.75 0 0 0 .515 1.076 32.91 32.91 0 0 0 3.256.508 3.5 3.5 0 0 0 6.972 0 32.903 32.903 0 0 0 3.256-.508.75.75 0 0 0 .515-1.076A11.448 11.448 0 0 1 16 8a6 6 0 0 0-6-6ZM8.05 14.943a33.54 33.54 0 0 0 3.9 0 2 2 0 0 1-3.9 0Z"
+				d="M5.25 9a6.75 6.75 0 0 1 13.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 0 1-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 1 1-7.48 0 24.585 24.585 0 0 1-4.831-1.244.75.75 0 0 1-.298-1.205A8.217 8.217 0 0 0 5.25 9.75V9Zm4.502 8.9a2.25 2.25 0 1 0 4.496 0 25.057 25.057 0 0 1-4.496 0Z"
 				clip-rule="evenodd" />
 		</svg>
+
 		<span
 			class="border shadow pointer-events-none select-none absolute left-full top-1/2 -translate-y-1/2 ml-2 w-max bg-white text-black text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition font-bold whitespace-nowrap z-10">
 			Notifications
