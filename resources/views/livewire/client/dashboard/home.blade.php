17330c2245ea4beab78743c86a254b3c299af4e6
JohnGabb12 <johngabvalencerina@gmail.com>
2025-10-03 14:22:34 +0800

    tweak/clock-icon

--- resources/views/livewire/client/dashboard/home.blade.php
+++ resources/views/livewire/client/dashboard/home.blade.php
@@ -100,11 +100,11 @@
                                     <span class="text-sm font-medium text-gray-600">Syllabus #{{ $syllabus->id }}</span>
                                 </div>
                                 <span class="px-2 py-1 text-xs rounded-full {{ 
-                                                                        $syllabus->status === 'approved' ? 'bg-green-100 text-green-800' :
+                                                                                                $syllabus->status === 'approved' ? 'bg-green-100 text-green-800' :
                 ($syllabus->status === 'under_review' ? 'bg-yellow-100 text-yellow-800' :
                     ($syllabus->status === 'rejected' ? 'bg-red-100 text-red-800' :
                         'bg-gray-100 text-gray-800')) 
-                                                                    }}">
+                                                                                            }}">
                                     {{ ucfirst(str_replace('_', ' ', $syllabus->status)) }}
                                 </span>
                             </div>
@@ -123,11 +123,13 @@
 
                             <div class="flex items-center justify-between pt-3 border-t">
                                 <div class="flex items-center space-x-2">
-                                    <svg class="w-4 h-4 text-gray-400" viewBox="0 0 24 24" fill="currentColor"
-                                        aria-hidden="true">
-                                        <path d="M12 6v6h5a1 1 0 010 2H11a1 1 0 01-1-1V6a1 1 0 112 0z" />
-                                        <path d="M12 2a10 10 0 100 20 10 10 0 000-20z" fill-opacity=".1" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                        class="size-5 text-gray-400">
+                                        <path fill-rule="evenodd"
+                                            d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-13a.75.75 0 0 0-1.5 0v5c0 .414.336.75.75.75h4a.75.75 0 0 0 0-1.5h-3.25V5Z"
+                                            clip-rule="evenodd" />
                                     </svg>
+
                                     <span class="text-xs text-gray-500">{{ $syllabus->updated_at->diffForHumans() }}</span>
                                 </div>
                                 <div class="flex items-center gap-4">
